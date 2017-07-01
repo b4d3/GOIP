e2fcf30d333cb5809d574122886dae68867d8ab4
root <ivan.brko@outlook.com>
2017-07-01 20:21:12 +0200

    Changed the python server code to match with the latest version of the ncl code

--- python_server_prototype/server.py
+++ python_server_prototype/server.py
@@ -1,8 +1,8 @@
 #A small python server program that excanges the data between peers!
 
-import socket    
+import socket
 
-def get_data(socket, client):
+def get_data(s, client):
     message, address = s.recvfrom(1024)
     client.append(address)
     print(client)
@@ -11,32 +11,30 @@ def get_data(socket, client):
 def infinite_message_router(s, peers):
     while True:
         message, address = s.recvfrom(1024)
+        print "peers0 .. " + str(peers[0])
+        print "peers1 .. " + str(peers[1])
+        print "address .. " + str(address)
+        print message
         if address != peers[0]:
             s.sendto(message, peers[0])
         else:
-            s.sendto(message, peers[0])
+            s.sendto(message, peers[1])
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)        
-host = socket.gethostname() 
-port = 12345                
-s.bind((host, port))        
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+host = socket.gethostname()
+port = 12345
+s.bind((host, port))
 
 peers = []
 
 get_data(s, peers)
 get_data(s, peers)
 
+
+s.sendto("Connection to peer established!", peers[0])
+s.sendto("Connection to peer established!", peers[1])
+
 infinite_message_router(s, peers)
-
-
-#print (peers[0])
-#print (peers[1])
-
-#s.sendto(str(peers[0]) + "\n", peers[1])
-#s.sendto(str(peers[1]) + "\n", peers[0])
-
-
-
 
 
 print ("End of execution")
